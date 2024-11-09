fed3687d8e3f3269e4a48b1a04faa4e93f210996
chandu <kasinenichandu@gmail.com>
2024-11-09 15:38:57 +0530

    added the changes

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
-  location = "East us"
+# vpc.tf
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"  # Adjust CIDR block as needed
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "MyVPC"
+    Environment = "dev"
+  }
 }
--- providers.tf
+++ providers.tf
@@ -1,16 +1,12 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.75.0"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
+provider "aws" {
+  # Configuration options
 }
